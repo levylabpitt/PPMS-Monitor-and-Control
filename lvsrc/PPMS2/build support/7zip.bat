590275735485f0c81e32ab3e10b8836f566b73ec
Patrick Irvin <p.irvin@levylab.org>
2019-10-23 12:54:58 -0400

    more rearranging and organizing new build scripts

--- lvsrc/PPMS2/build support/7zip.bat
+++
@@ -1,3 +0,0 @@
-set PATH=%PATH%;C:\Program Files\7-Zip\
-cd "C:\user\patrick\hg\levylab_inst_ppms2\builds\7z Install"
-copy /b 7zS2.sfx + config.txt + Volume.7z "..\PPMS2 Monitor and Control-1.3.4.18-install.exe"
